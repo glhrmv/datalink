fa28fea36a81d0dd24e9907aa4df757c4014363f
Luís Marques <ei11159@fe.up.pt>
2019-10-02 10:19:49 +0100

    initial commit

--- writenoncanonical.c
+++ writenoncanonical.c
@@ -11,25 +11,57 @@
 #define _POSIX_SOURCE 1 /* POSIX compliant source */
 #define FALSE 0
 #define TRUE 1
+#define FLAG 0b01111110
+#define A 0b00000011
+#define C 0b00000011
+#define BCC A^C
+#define SIZE 255
 
 volatile int STOP=FALSE;
+
+int llopen(int *fd, char *b){
+  *fd = open(b, O_RDWR | O_NOCTTY );
+  if (*fd <0) {perror(argv[1]); exit(-1); }
+
+}
+
+int llwrite(int fd, char * buffer, int length){
+  int res = 0;
+  res = write(fd, buffer, length);
+
+  return res;
+}
+
+int llread(int fd, char * buffer){
+  int res = 0;
+  res = read(fd,buffer,SIZE);
+  return res;
+}
+
+
 
 int main(int argc, char** argv)
 {
     int fd,c, res;
     struct termios oldtio,newtio;
-    char buf[255];
+    unsigned char frame[SIZE];
     int i, sum = 0, speed = 0;
-    
-    if ( (argc < 2) || 
-  	     ((strcmp("/dev/ttyS0", argv[1])!=0) && 
+
+    if ( (argc < 2) ||
+  	     ((strcmp("/dev/ttyS0", argv[1])!=0) &&
   	      (strcmp("/dev/ttyS1", argv[1])!=0) )) {
       printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
       exit(1);
     }
 
-    fd = open(argv[1], O_RDWR | O_NOCTTY );
-    if (fd <0) {perror(argv[1]); exit(-1); }
+
+  /*
+    Open serial port device for reading and writing and not as controlling tty
+    because we don't want to get killed if linenoise sends CTRL-C.
+  */
+
+
+    llopen(&fd, argv[1]);
 
     if ( tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
       perror("tcgetattr");
@@ -39,36 +71,52 @@ int main(int argc, char** argv)
     bzero(&newtio, sizeof(newtio));
     newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
     newtio.c_iflag = IGNPAR;
-    newtio.c_oflag = OPOST;
+    newtio.c_oflag = 0;
 
     /* set input mode (non-canonical, no echo,...) */
     newtio.c_lflag = 0;
 
     newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
-    newtio.c_cc[VMIN]     = 5;   /* blocking read until 5 chars received */
+    newtio.c_cc[VMIN]     = 1;   /* blocking read until 5 chars received */
 
 
-    tcflush(fd, TCIFLUSH);
+
+  /*
+    VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a
+    leitura do(s) pr�ximo(s) caracter(es)
+  */
+
+
+
+    tcflush(fd, TCIOFLUSH);
 
     if ( tcsetattr(fd,TCSANOW,&newtio) == -1) {
       perror("tcsetattr");
       exit(-1);
     }
 
-    for (i = 0; i < 255; i++) {
-      buf[i] = 'a';
-    }
-    
-    /*testing*/
-    buf[25] = '\n';
-    
-    res = write(fd,buf,255);   
+    printf("New termios structure set\n");
+
+
     printf("%d bytes written\n", res);
-    
+
+
+  /*
+    O ciclo FOR e as instru��es seguintes devem ser alterados de modo a respeitar
+    o indicado no gui�o
+  */
+
+
+
+
     if ( tcsetattr(fd,TCSANOW,&oldtio) == -1) {
       perror("tcsetattr");
       exit(-1);
     }
+
+
+
+
     close(fd);
     return 0;
 }
