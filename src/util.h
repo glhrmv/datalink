837d916c678fe9a2b3c317bb9b40c883d838ea04
glhrmv <gvmgui@gmail.com>
2019-10-11 12:19:03 +0100

    Add util.h

---
+++ src/util.h
@@ -0,0 +1,14 @@
+/**
+ * @file util.h
+ * @brief General project-independent helper macros
+ * 
+ */
+
+#pragma once
+
+#define TRUE 1
+#define FALSE 0
+
+#define MAX_SIZE 256
+
+#define BIT(n) (0x01 << n)
