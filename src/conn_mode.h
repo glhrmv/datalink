bb20fa21b84e9bfce25772dce57d2e629c17053f
glhrmv <gvmgui@gmail.com>
2019-10-11 12:18:58 +0100

    Add conn_mode.h

---
+++ src/conn_mode.h
@@ -0,0 +1,16 @@
+/**
+ * @file conn_mode.h
+ * @brief Connection mode enum definition
+ * 
+ */
+
+/**
+ * @brief Connection mode enum
+ * 
+ * SEND is to be used by the sender.
+ * RECEIVE is to be used by the receiver.
+ * 
+ */
+typedef enum conn_mode {
+	SEND, RECEIVE
+} conn_mode;
