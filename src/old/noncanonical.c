6ebf0bb1d48ae04410a141a8d3e09f7afa098deb
glhrmv <gvmgui@gmail.com>
2019-10-11 17:08:32 +0100

    Move current source files to old/

--- src/old/noncanonical.c
+++ src/old/noncanonical.c
@@ -168,12 +168,6 @@ int llclose(int fd) {
 }
 
 int main(int argc, char **argv) {
-  if ((argc < 2) || ((strcmp("/dev/ttyS0", argv[1]) != 0) &&
-                     (strcmp("/dev/ttyS1", argv[1]) != 0))) {
-    printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
-    exit(1);
-  }
-
   unsigned char UA[5];
   unsigned char frame[SIZE];
   char buf[SIZE];
