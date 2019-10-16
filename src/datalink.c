7b9e7a749b4b81f004ef855ba0f4de4197c6748c
glhrmv <gvmgui@gmail.com>
2019-10-16 09:56:28 +0100

    Add basic arg parsing

--- src/datalink.c
+++ src/datalink.c
@@ -1,23 +1,35 @@
 /**
  * @file datalink.c
  * @brief The datalink program starting point
- * 
+ *
  */
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
 #include <string.h>
 
+#include "util.h"
 #include "ll.h"
 
+conn_mode get_conn_mode(const char *cm) {
+  if (strcmp(cm, "send") == 0)
+    return SEND;
+  else if (strcmp(cm, "receive") == 0)
+    return RECEIVE;
+  
+  printf("Invalid connection mode. Must be 'send' or 'receive'\n");
+
+  return 1;
+}
+
 int main(int argc, char **argv) {
-  if ((argc < 2) || ((strcmp("/dev/ttyS0", argv[1]) != 0) &&
-                     (strcmp("/dev/ttyS1", argv[1]) != 0))) {
-    printf("Usage:\t%s <serial port>\ne.g.:\t%s /dev/ttyS1\n", argv[0], argv[0]);
-    exit(1);
+  if (argc != 3) {
+    printf("Usage: %s <send|receive> <serial port number>\n", argv[0]);
+    return 1;
   }
 
-  printf("hello world\n");
+  conn_mode cm = get_conn_mode(argv[1]);
 
   return 0;
 }
