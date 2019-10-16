97788bbc24683bd5fdf550d4eb45c09420cb747d
Luís Marques <ei11159@fe.up.pt>
2019-10-16 08:58:47 +0100

    Add files via upload

--- src/old/writenoncanonical.c
+++ src/old/writenoncanonical.c
@@ -5,16 +5,23 @@
 #include <fcntl.h>
 #include <termios.h>
 #include <stdio.h>
+#include <signal.h>
 
 #define BAUDRATE B38400
 #define MODEMDEVICE "/dev/ttyS1"
 #define _POSIX_SOURCE 1 /* POSIX compliant source */
 #define FALSE 0
 #define TRUE 1
+
+
 #define FLAG 0x7e
 #define A 0x03
-#define C 0x03
-#define N 5
+
+
+#define C_SET 0x03
+#define C_UA 0x07 // Valor do UA
+#define C_DISC 0x011
+#define N 5    // Tamanho trama SET
 
 //State machine
 #define START     0
@@ -27,18 +34,23 @@
 
 #define SIZE 255
 
-#define TRANSMITTER 0
-#define RECEIVER 1
-
 volatile int STOP_=FALSE;
 
 
+int counter = 1;
+
 struct termios oldtio,newtio;
 
-int llopen(int porta, int flag){
+void atende() {
+  printf("contador # %d\n", counter);
+  counter++;
+}
 
-  int fd;
-  if(flag == TRANSMITTER){
+int llopen(char* porta){
+    int fd;
+    char buffer[5];
+    unsigned char frame_S[] = {FLAG, A, C_SET, A^C_SET, FLAG};
+
     fd = open(porta, O_RDWR | O_NOCTTY );
     if (fd <0) {perror(porta); return -1; }
 
@@ -64,112 +76,138 @@ int llopen(int porta, int flag){
       perror("tcsetattr");
       exit(-1);
     }
-  }
-  else if(flag == RECEIVER){
-    fd = open(porta, O_RDWR | O_NOCTTY );
-    if (fd <0) {perror(porta); return -1; }
-  }
 
-  return fd;
+    printf("New termios structure set\n");
+
+    write(fd, frame_S, strlen(frame_S));
+
+    read(fd, buffer, N);
+
+    if (stateMachine(buffer) != 0){
+      exit(-1);
+    }
+
+    return fd;
+}
+
+unsigned char calculateBcc( char *data) {
+  unsigned char bcc = data[0];
+  for(int i = 1; i < strlen(data); i++) {
+    bcc ^= data[i];
+  }
+  return bcc;
 }
 
 int llwrite(int fd, char * buffer, int length){
-  return write(fd, buffer, length);
+  unsigned char c = 0x00;
+  unsigned char bcc2 = calculateBcc(buffer);
+  unsigned char buff[5];
+  unsigned char frame_I[] = {FLAG, A, c, A^c, buffer, bcc2, FLAG};
+
+  write(fd, buffer, length);
+
+  read(fd, buff, 5);
+
+  c = 0x40;
 }
 
+
+
 int llread(int fd, char * buffer){
+  
+}
+
+int stateMachine(char *buffer) {
   int state = 0;
-  char flag1, flag2, a, c;
+  char flag1, flag2, a, c, bcc;
+
+  flag1 = buffer[0];
+  a = buffer[1];
+  c = buffer[2];
+  bcc = buffer[3];
+  flag2 = buffer[4];
+  
   while(TRUE)
   {
     switch (state) {
-
       //START*******************************
       case START:
-        if(read(fd, &flag1, 1))
-        {
-          if(flag1 == FLAG)
-            state = FLAG_RCV;
-        }
+        if(flag1 == FLAG)
+          state = FLAG_RCV;
         else
-          state = Other_RCV;
-        break;
+          state = START;
+      break;
 
       //FLAG_RCV****************************
       case FLAG_RCV:
-        if(read(fd, &a, 1))
-        {
-          if(a == FLAG)
-            state = FLAG_RCV;
-          else
+        if(a == FLAG)
+          state = FLAG_RCV;
+        else if(a == A)
           state = A_RCV;
-        }
         else
-          state = Other_RCV;
-        break;
+          state = START;
+      break;
 
       //A_RCV*******************************
       case A_RCV:
-      if(read(fd, &c, 1))
-      {
         if(c == FLAG)
           state  = FLAG_RCV;
+        else if(c == C_UA || c == C_DISC)
+          state = C_RCV;
         else
-        state = C_RCV;
-      }
-      else
-      state = Other_RCV;
+          state = START;
       break;
-
 
       //C_RCV********************************
       case C_RCV:
-        if(read(fd, buffer, 1))
-        {
-          if(a^c == buffer[0])
-            state = BCC_OK;
-          else
-          if(buffer[0] == FLAG)
-            state = FLAG_RCV;
-        }
+        if(a^c == bcc)
+          state = BCC_OK;
+        else if(a^c == FLAG)
+          state = FLAG_RCV;
         else
-          state = Other_RCV;
-        break;
+          state = START;
+      break;
 
       //BCC_OK********************************
       case BCC_OK:
-        if(read(fd, &flag2, 1))
-        {
-          if(flag2 == FLAG)
-            state = STOP;
-        }
+        if(flag2 == FLAG)
+          state = STOP;
         else
-          state = Other_RCV;
-        break;
+         state = START;
+      break;
 
       //STOP***********************************
       case STOP:
         return 0;
 
-
-      //Other_RCV******************************
-      case Other_RCV:
-        state = START;
-        break;
-
       //default********************************
       default:
-      return 1;
+        return 1;
     }
   }
 }
 
-int close(int fd){
+int llclose(int fd){
+  unsigned char frame_D[] = {FLAG, A, C_DISC, A^C_DISC, FLAG};
+  unsigned char frame_U[] = {FLAG, A, C_UA, A^C_UA, FLAG};
+  unsigned char buff[5];
+
+  write(fd, frame_D, strlen(frame_D));
+
+  read(fd, buff, 5);
+
+  if (stateMachine(buff) != 0) {
+    perror("Error disconect\n");
+    return -1;
+  }
+
+  write(fd, frame_U, strlen(frame_U));
+
   if ( tcsetattr(fd,TCSANOW,&oldtio) == -1) {
       perror("tcsetattr");
       return -1;
   }
-  
+
   return close(fd);
 }
 
@@ -181,11 +219,13 @@ int main(int argc, char** argv)
     unsigned char SET[N];
 
     if ( (argc < 2) ||
-  	     ((strcmp("/dev/ttyS0", argv[1])!=0) &&
+  	     ((strcmp("/dev/ttyS2", argv[1])!=0) &&
   	      (strcmp("/dev/ttyS1", argv[1])!=0) )) {
       printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
       exit(1);
     }
+
+    (void) signal(SIGALRM, atende);
 
 
   /*
@@ -193,30 +233,26 @@ int main(int argc, char** argv)
     because we don't want to get killed if linenoise sends CTRL-C.
   */
 
-    fd = llopen(argv[1], TRANSMITTER);
+    
+
+    fd = llopen(argv[1]);
     if(fd < 0){
       printf("Error llopen\n");
       exit(1);
     }
-
-    printf("New termios structure set\n");
-
-
-    SET[0] = FLAG;
-    SET[1] = A;
-    SET[2] = C;
-    SET[3] = A^C;
-    SET[4] = FLAG;
-
-    if(llwrite(fd, SET, N) < 0){
-      printf("Error llwrite\n");
-      exit(1);
+    else {
+      sleep(0);
+      STOP_ = TRUE;
     }
 
-    if(llread(fd, buf) != 0){
-      printf("Error llread\n");
-      exit(1);
-    }
+    FILE *file = fopen("pinguim.gif", "r");
+
+    fseek(file, 0, SEEK_END);
+    unsigned long file_size = ftell(file);
+
+    llwrite(fd, file, file_size);
+
+    
 
     gets(buf);
 
@@ -225,7 +261,6 @@ int main(int argc, char** argv)
     res = write(fd,buf,strlen(buf)+1);
     printf("%d bytes written\n", res);
 
-    
 
     if(llclose(fd) != 0){
       printf("Error llclose\n");
